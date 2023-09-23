eb5a5145b66085d9e7e1c3aee9ce02ecb5e05756
valery445 <valer4ii@gmail.com>
2023-09-23 20:52:39 +0300

    made unsigned registry summation

--- virtual_processor.go
+++ virtual_processor.go
@@ -2,8 +2,8 @@ package main
 
 func main() {
 	r1, r2 := NewRegistry(), NewRegistry()
-	r1.setBit(7, true)
-	r2.setBit(7, true)
+	r1.setByteNumber("00000001")
+	r2.setByteNumber("11111111")
 	r1.printRegistry()
 	r2.printRegistry()
 	r3 := Sum(r1, r2)
@@ -19,10 +19,22 @@ type memoryRegistry struct {
 	cells []bool
 }
 
-func Sum(r1 *memoryRegistry, r2 *memoryRegistry) *memoryRegistry {
+func Sum(a *memoryRegistry, b *memoryRegistry) *memoryRegistry {
 	r3 := NewRegistry()
-	r3.cells[6] = r1.cells[7] && r2.cells[7]
-	r3.cells[7] = r1.cells[7] != r2.cells[7]
+	pCurrent := false
+	var pNext bool
+	for i := 7; i >= 0; i-- {
+		var s bool
+		if !pCurrent {
+			s = a.getBit(i) != b.getBit(i)
+			pNext = a.getBit(i) && b.getBit(i)
+		} else {
+			s = a.getBit(i) == b.getBit(i)
+			pNext = a.getBit(i) || b.getBit(i)
+		}
+		r3.setBit(i, s)
+		pCurrent = pNext
+	}
 	return r3
 }
 
@@ -42,6 +54,34 @@ func NOR(x, y bool) bool {
 
 func (registry *memoryRegistry) setBit(index int, b bool) {
 	registry.cells[index] = b
+}
+
+func (registry *memoryRegistry) setByteNumber(byteNumber string) {
+	if isByteNumber(byteNumber) {
+		for i := 0; i < 8; i++ {
+			if byteNumber[i] == '0' {
+				registry.setBit(i, false)
+			} else {
+				registry.setBit(i, true)
+			}
+		}
+	}
+}
+
+func isByteNumber(byteNumber string) bool {
+	if len(byteNumber) != 8 {
+		return false
+	}
+	for _, bit := range byteNumber {
+		if bit != '0' && bit != '1' {
+			return false
+		}
+	}
+	return true
+}
+
+func (registry *memoryRegistry) getBit(index int) bool {
+	return registry.cells[index]
 }
 
 func (r memoryRegistry) printRegistry() {
