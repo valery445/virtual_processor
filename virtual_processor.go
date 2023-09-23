dbb4b1873e90220d77e5e36882a9044e25c962fa
valery445 <valer4ii@gmail.com>
2023-09-23 19:58:48 +0300

    reworked memoryRegistry - now don't need triggers

--- virtual_processor.go
+++ virtual_processor.go
@@ -1,19 +1,13 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	r := NewRegistry()
-	r.printRegistry()
-	r.trigger(0, true, false)
-	r.trigger(2, true, false)
-	r.printRegistry()
-	r.trigger(0, false, false)
-	r.trigger(2, false, false)
-	r.printRegistry()
-	//printTruthTable(NOR)
+	r1, r2 := NewRegistry(), NewRegistry()
+	r1.setBit(7, true)
+	r2.setBit(7, true)
+	r1.printRegistry()
+	r2.printRegistry()
+	r3 := Sum(r1, r2)
+	r3.printRegistry()
 }
 
 // && - AND
@@ -21,24 +15,23 @@ func main() {
 // ! - NOT
 // != XOR
 
-type logicalOperation func(bool, bool) bool
-
-type memoryCell struct {
-	Q  bool
-	Q_ bool
+type memoryRegistry struct {
+	cells []bool
 }
 
-type memoryRegistry struct {
-	cells []memoryCell
+func Sum(r1 *memoryRegistry, r2 *memoryRegistry) *memoryRegistry {
+	r3 := NewRegistry()
+	r3.cells[6] = r1.cells[7] && r2.cells[7]
+	r3.cells[7] = r1.cells[7] != r2.cells[7]
+	return r3
 }
 
 func NewRegistry() *memoryRegistry {
 	r := &memoryRegistry{
-		cells: make([]memoryCell, 8),
+		cells: make([]bool, 8),
 	}
 	for i := 0; i < len(r.cells); i++ {
-		r.cells[i].Q = false
-		r.cells[i].Q_ = true
+		r.cells[i] = false
 	}
 	return r
 }
@@ -47,31 +40,14 @@ func NOR(x, y bool) bool {
 	return !(x || y)
 }
 
-func printTruthTable(op logicalOperation) {
-	fmt.Println(op(false, false))
-	fmt.Println(op(false, true))
-	fmt.Println(op(true, false))
-	fmt.Println(op(true, true))
-}
-
-func (c *memoryCell) trigger(s bool, r bool) {
-	if s != r {
-		c.Q = s
-		c.Q_ = r
-	}
-}
-
-func (registry *memoryRegistry) trigger(index int, s bool, r bool) {
-	if s != r {
-		registry.cells[index].Q = s
-		registry.cells[index].Q_ = r
-	}
+func (registry *memoryRegistry) setBit(index int, b bool) {
+	registry.cells[index] = b
 }
 
 func (r memoryRegistry) printRegistry() {
 	var binary string
-	for _, v := range r.cells {
-		if v.Q {
+	for _, bit := range r.cells {
+		if bit {
 			binary += "1"
 		} else {
 			binary += "0"
